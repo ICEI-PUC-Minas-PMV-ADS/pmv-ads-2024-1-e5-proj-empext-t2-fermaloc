f0e7506c730ce7b3b79900b5af2ac6568e3dbef9
Igor Costa <100178056+Igortdcosta@users.noreply.github.com>
2024-03-27 21:57:55 -0300

    Alteração CSS index main e index login e criação de novas funções no Login

--- codigo-fonte/frontend/fermaloc/src/pages/Admin/Login/index.js
+++ codigo-fonte/frontend/fermaloc/src/pages/Admin/Login/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { login } from '../../../services/authenticationService.js';
 import "./styles.css";
+import { MdEmail, MdLock } from "react-icons/md";
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -18,15 +19,40 @@ export default function Login() {
   return (
     <div>
         {/* Código HTML */}
-        <form>
-          <input className='' type="text" name="email" value={email} 
-          onChange={(e) => setEmail(e.target.value)}
-          />
-          <input className='' type="password" name="password" value={password} 
-          onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className='' type="submit" onClick={(e) => submitForm(e)}>Login</button>
-        </form>
+          <div className="wrapper">
+            <form action="">
+
+              <h1>Login</h1>
+
+              <div className="input-box">
+                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} 
+                required/>
+                <MdEmail className="icon" />
+              </div>
+
+              <div className="input-box">
+                <input type="password" placeholder="Password" value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                required/>
+                <MdLock className="icon" />
+              </div>
+
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" name="remember" /> Lembre-me
+                </label>
+                <a href="#">Esqueceu a senha?</a>
+              </div>
+
+              <button className='' type="submit" onClick={(e) => submitForm(e)}>Login</button>
+
+              <div className="register-link">
+                <p>Não tem uma conta? <a href="#">Registre-se</a></p>
+                
+              </div>
+
+            </form>
+          </div>   
     </div>
   )
 }
