5fa305f65bc3bbb22a990777b46f9434971ab394
Igor Costa <igorteixeiradacosta@gmail.com>
2024-04-24 00:02:32 -0300

    Criação das Rotas - ADMIN

--- codigo-fonte/frontend/fermaloc/src/pages/Admin/Home/index.js
+++ codigo-fonte/frontend/fermaloc/src/pages/Admin/Home/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import useAuthentication from "../../../hooks/useAuthentication";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 export default function HomeAdmin() {
   const { authenticated } = useAuthentication();
-  return <>{authenticated && <div>Login feito com sucesso!</div>}</>;
+  return <>{authenticated && 
+  
+  <div className={styles.classButton}>
+    <div>
+      <Link to="/admin/banners">Banners</Link>
+    </div>
+    <div>
+      <Link to="/admin/categorias">Categorias</Link>
+    </div>
+    <div>
+      <Link to="/admin/produtos">Products</Link>
+    </div>
+
+  </div>
+
+  
+  }</>;
 }
