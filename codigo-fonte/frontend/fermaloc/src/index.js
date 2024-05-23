ec36915128352434e2cac86acbf594861bc91044
Vicinius115 <viciniuseduardo887@gmail.com>
2024-05-22 22:22:40 -0300

    trocando o nome title bar e img

--- codigo-fonte/frontend/fermaloc/src/index.js
+++ codigo-fonte/frontend/fermaloc/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import RoutesComponent from './routes';
 
 
+  <title>fermaloc</title>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
