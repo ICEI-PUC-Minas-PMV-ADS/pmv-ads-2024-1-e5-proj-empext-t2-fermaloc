27d8ba0a55f1eb47a8f2e4066d6de5b510d512bf
Vicinius115 <viciniuseduardo887@gmail.com>
2024-06-09 19:02:27 -0300

    correções do texto da page aboutus e subindo o package da função wpp

--- codigo-fonte/frontend/fermaloc/src/pages/User/Aboutus/index.js
+++ codigo-fonte/frontend/fermaloc/src/pages/User/Aboutus/index.js
@@ -34,11 +34,11 @@ export default function AboutUs() {
           outras máquinas.
         </p>
         <p>
-          Sendo assim, nos colocamos a inteira disposição para prestar-lhes
+          Nos colocamos a inteira disposição para prestar
           serviços especializados de manutenção e/ou locação de máquinas e
-          ferramentas na certeza de oferecer-lhes um serviço de qualidade.
+          ferramentas com a garantia de um serviço de qualidade.
         </p>
-        <p>Sempre à disposição.</p>
+     
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3758.170723883018!2d-44.04823172456286!3d-19.61999762873054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6618551f89a9f%3A0x7c54a99cf097260b!2sFermaloc%20-%20Ferramentas%20El%C3%A9tricas%2C%20M%C3%A1quina%20e%20Loca%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1710905719512!5m2!1spt-BR!2sbr"
